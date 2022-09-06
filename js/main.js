39f15197ded68f80eebcd8d4c50cf8c4ce834feb
najmiddinabdullaev <najmiddin.9898@mail.ru>
2022-09-06 21:42:01 +0500

    Update active showcase img on thumbnail click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const modifiers = {
+  imgThumbnailActive: `img-showcase__thumbnail--active`
+};
+
 const elSiteHeaderCardLink = document.querySelector(`.js-site-header-card-link`);
 const elSiteHeaderCardModal = document.querySelector(`.site-header__card-modal`);
 
@@ -8,3 +12,21 @@ if (elSiteHeaderCardLink) {
     elSiteHeaderCardModal.classList.toggle(`site-header__card-modal--open`);
   });
 }
+
+const elImgShowcaseActiveImg = document.querySelector(`.img-showcase__active-img`);
+const elsImgShowCaseTmubnailButton = document.querySelectorAll(`.js-img-showcase-thumbnail-button`);
+const elsImgThumbnail = document.querySelectorAll(`.img-showcase__thumbnail`);
+
+elsImgShowCaseTmubnailButton.forEach(function (elButton) {
+  elButton.addEventListener(`click`, function () {
+
+  elsImgThumbnail.forEach(function (elImgThumbnail) {
+    elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+  });
+
+  elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+  elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig;
+  elImgShowcaseActiveImg.srcset =`${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseBig} 2x`;
+  });
+});
