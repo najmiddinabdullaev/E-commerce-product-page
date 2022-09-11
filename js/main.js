a9e3aee2f98ef01ec56eec88179158f05eedfc47
najmiddinabdullaev <najmiddin.9898@mail.ru>
2022-09-11 23:32:04 +0500

    Add product info count button js

--- js/main.js
+++ js/main.js
@@ -131,3 +131,26 @@ if (elLightboxControlPrev) {
     elImgLightboxActiveImg.srcset = `${elNextActiveItem.children[0].dataset.imgShowcaseBig} 1x, ${elNextActiveItem.children[0].dataset.imgShowcaseRetina} 2x`;
   });
 }
+
+
+// PRODUCT COUNT
+
+const elProductQtyDecrementButton = document.querySelector(`.js-product-quantity-decrement-button`);
+const elProductQtyIncrementButton = document.querySelector(`.js-product-quantity-increment-button`);
+const elProductQty = document.querySelector(`.product-info__quantity`);
+
+if(elProductQtyIncrementButton) {
+  elProductQtyIncrementButton.addEventListener(`click`, function () {
+    elProductQty.textContent = parseInt(elProductQty.textContent, 10) + 1;
+  });
+}
+
+if(elProductQtyDecrementButton) {
+  elProductQtyDecrementButton.addEventListener(`click`, function () {
+    const qty = parseInt(elProductQty.textContent, 10);
+
+    if (qty > 0) {
+      elProductQty.textContent = qty - 1;
+    }
+  });
+}
